aa7ef85f33f7c4b30bf39e725b218520ab8e1721
sescandon <sescandon@unal.edu.co>
2024-11-03 11:58:38 -0500

    Initialization commit

---
+++ Práctica 2 - Problema de las jarras/Práctica 2 - Problema de las jarras/Jar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica_2___Problema_de_las_jarras
+{
+    public class Jar
+    {
+        private int capacity;
+        private int currentVolume;
+
+        public Jar(int capacity)
+        {
+            this.capacity = capacity;
+            this.currentVolume = 0;
+        }
+
+        public int getCapacity()
+        {
+            return this.capacity;
+        }
+
+        public int getCurrentVolume()
+        {
+            return this.currentVolume;
+        }
+
+        public void empty()
+        {
+            this.currentVolume = 0;
+        }
+
+        public void fill()
+        {
+            this.currentVolume = capacity;
+        }
+
+        public void fill(Jar other)
+        {
+            var otherVolume = other.getCurrentVolume();
+
+            if(otherVolume + this.currentVolume >= this.capacity)
+            {
+                otherVolume -= this.capacity - this.currentVolume;
+                this.currentVolume = this.capacity;
+                other.fill(otherVolume);
+            }
+            else
+            {
+                this.currentVolume += otherVolume;
+                other.empty();
+            }
+        }
+
+        private void fill(int volume)
+        {
+            this.currentVolume = volume;
+        }
+
+    }
+}
---
+++ Práctica 2 - Problema de las jarras/Práctica 2 - Problema de las jarras/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using Práctica_2___Problema_de_las_jarras;
+
+Jar jar1 = new Jar(3);
+Jar jar2 = new Jar(5);
+
+while (true)
+{
+    Console.WriteLine("1. Llenar jarra 1");
+    Console.WriteLine("2. Llenar jarra 2");
+    Console.WriteLine("3. Vaciar jarra 1");
+    Console.WriteLine("4. Vaciar jarra 2");
+    Console.WriteLine("5. Llenar jarra 1 con jarra 2");
+    Console.WriteLine("6. Llenar jarra 2 con jarra 1");
+    Console.WriteLine("7. Salir \n");
+
+    var option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            jar1.fill();
+            break;
+        case "2":
+            jar2.fill();
+            break;
+        case "3":
+            jar1.empty();
+            break;
+        case "4":
+            jar2.empty();
+            break;
+        case "5":
+            jar1.fill(jar2);
+            break;
+        case "6":
+            jar2.fill(jar1);
+            break;
+        case "7":
+            return;
+        default:
+            Console.WriteLine("Opción inválida");
+            break;
+    }
+
+    Console.Clear();
+
+    Console.WriteLine($"\nEl volumen de la jarra de {jar1.getCapacity()} litros es de {jar1.getCurrentVolume()}");
+    Console.WriteLine($"El volumen de la jarra de {jar2.getCapacity()} litros es de {jar2.getCurrentVolume()}\n");
+
+    if (jar1.getCurrentVolume() == 1 || jar2.getCurrentVolume() == 1)
+    {
+        Console.WriteLine("El reto fue completado");
+        break;
+    }
+
+}
+
